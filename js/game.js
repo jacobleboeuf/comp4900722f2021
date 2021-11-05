9062c5bb4ca3502b0db327c9f59c494a4e7949b8
Jacob Leboeuf <81262890+jacobleboeuf@users.noreply.github.com>
2021-11-05 00:22:14 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,127 +1,164 @@
-var lives = 3;
+/**
+ * File: game.js
+ * Computer Graphics II Project - Big Brain or LAME?
+ *
+ * - This project uses HTML, CSS, and JavaScript to create a brain teaser game made
+ *   to frustrate and challenge the user.
+ *
+ * - This file contains the JavaScript for the entire project, and is responsible
+ *   for all of the levels of the game, including the gameOver and Victory screen
+ *
+ * Jacob Leboeuf, UMass Lowell Computer Science, jacob_leboeuf@student.uml.edu,
+ * Copyright (c) 2021 by Jacob. All rights reserved. May be freely copied or
+ * excerpted for educational purposes with credit to the author.
+ * updated by JL on November 5, 2021 at 12:12 AM
+**/
+var lives = 3; // global variable for number of brains/lives in game
 var play = document.querySelector('.play');
 var display = document.getElementById("game");
 var time = document.createElement('div');
-time.className = 'time';
-var ampm;
+time.className = 'time'; // For non-clock updating time in level1
+var ampm; // for displaying AM/PM with times in level1
 var newText;
-let canvas, ctx, radius;
-var currDate;
-var timeStopped = false;
+let canvas, ctx, radius; // for making canvas clock
+var currDate; // global date variable
+var timeStopped = false; // boolean variable checking if clock was stopped in level1
+/**
+*  showRules() - Displays the rules for the game
+*
+*/
 play.addEventListener("click", showRules);
 function showRules() {
-  display.setAttribute("style", "background-image: none;");
-  display.innerHTML = "<h1 id=\"rules\">DA RULEZ</h1>";
-  display.innerHTML += "<p>The rules are simple:</p>"
-  var rules = document.createElement("ol");
-  var rule = document.createElement("li");
-  newText = document.createTextNode('Get through the various levels to prove you are ');
-  rule.appendChild(newText);
-  newText = document.createTextNode('BIG BRAIN');
-  var span = document.createElement('span');
-  span.style = 'color: green';
-  span.appendChild(newText);
-  rule.appendChild(span);
-  rules.appendChild(rule);
-  rule = document.createElement("li");
-  newText = document.createTextNode('If you fail to pass a level, you will lose a brain');
-  rule.appendChild(newText);
-  rules.appendChild(rule);
-  rule = document.createElement("li");
-  newText = document.createTextNode('You have 3 brains. If you run out of brains, you are ');
-  rule.appendChild(newText);
-  newText = document.createTextNode('LAME');
-  span = document.createElement('span');
-  span.style = 'color: red';
-  span.appendChild(newText);
-  rule.appendChild(span);
-  rules.appendChild(rule);
-  display.appendChild(rules);
-  display.innerHTML += "<p> Are you ready? Click 'begin' to begin!</p>";
-  var button = document.createElement("button");
-  button.className = 'play';
-  button.innerHTML = "BEGIN";
-  button.addEventListener("click", level1);
-  display.appendChild(button);
+    display.setAttribute("style", "background-image: none;");
+    display.innerHTML = "<h1 id=\"rules\">DA RULEZ</h1>";
+    display.innerHTML += "<p>The rules are simple:</p>"
+    var rules = document.createElement("ol");
+    var rule = document.createElement("li");
+    newText = document.createTextNode('Get through the various levels to prove you are ');
+    rule.appendChild(newText);
+    newText = document.createTextNode('BIG BRAIN');
+    var span = document.createElement('span');
+    span.style = 'color: green';
+    span.appendChild(newText);
+    rule.appendChild(span);
+    rules.appendChild(rule);
+    rule = document.createElement("li");
+    newText = document.createTextNode('If you fail to pass a level, you will lose a brain');
+    rule.appendChild(newText);
+    rules.appendChild(rule);
+    rule = document.createElement("li");
+    newText = document.createTextNode('You have 3 brains. If you run out of brains, you are ');
+    rule.appendChild(newText);
+    newText = document.createTextNode('LAME');
+    span = document.createElement('span');
+    span.style = 'color: red';
+    span.appendChild(newText);
+    rule.appendChild(span);
+    rules.appendChild(rule);
+    display.appendChild(rules);
+    display.innerHTML += "<p> Are you ready? Click 'begin' to begin!</p>";
+    var button = document.createElement("button");
+    button.className = 'play';
+    button.innerHTML = "BEGIN";
+    button.addEventListener("click", level1);
+    display.appendChild(button);
 }
-// https://www.w3schools.com/graphics/canvas_clock.asp
+/**
+* drawClock() - uses helper functions to draw canvas clock
+* Heavy inspiration from https://www.w3schools.com/graphics/canvas_clock.asp
+*/
 function drawClock() {
-  drawFace(ctx, radius);
-  drawNumbers(ctx, radius);
-  if (timeStopped) {
-      drawTime(ctx, radius, currDate);
-  } else {
-      drawTime(ctx, radius, new Date());
+    drawClockFace(ctx, radius);
+    drawClockNumbers(ctx, radius);
+    if (timeStopped) {
+        drawTime(ctx, radius, currDate);
+    } else {
+        drawTime(ctx, radius, new Date());
+    }
+}
+/**
+* drawClockFace(ctx, radius) - helper function to drawClock that creates the clock face
+* Heavy inspiration from https://www.w3schools.com/graphics/canvas_clock_face.asp
+*/
+function drawClockFace(ctx, radius) {
+    ctx.beginPath();
+    ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+    ctx.fillStyle = 'white';
+    ctx.fill();
+    var gradient = ctx.createRadialGradient(0, 0 ,radius * 0.95, 0, 0, radius * 1.05);
+    gradient.addColorStop(0, 'black');
+    ctx.lineWidth = radius*0.1;
+    ctx.strokeStyle = gradient;
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
+    ctx.fillStyle = 'black';
+    ctx.fill();
+}
+/**
+* drawClockNumbers(ctx, radius) - helper function to drawClock that creates the clock numbers
+* Heavy inspiration from https://www.w3schools.com/graphics/canvas_clock_numbers.asp
+*/
+function drawClockNumbers(ctx, radius) {
+    var angle;
+    ctx.textBaseline = "middle";
+    ctx.textAlign = "center";
+    ctx.font = "bold " + radius * 0.15 + "px arial";
+    for(var i = 1; i <= 12; i++) {
+        angle = i * Math.PI / 6;
+        ctx.rotate(angle);
+        ctx.translate(0, -radius * 0.85);
+        ctx.rotate(-angle);
+        ctx.fillText(i.toString(), 0, 0);
+        ctx.rotate(angle);
+        ctx.translate(0, radius * 0.85);
+        ctx.rotate(-angle);
   }
 }
-
-function drawFace(ctx, radius) {
-  var grad;
-  ctx.beginPath();
-  ctx.arc(0, 0, radius, 0, 2 * Math.PI);
-  ctx.fillStyle = 'white';
-  ctx.fill();
-
-  grad = ctx.createRadialGradient(0, 0 ,radius * 0.95, 0, 0, radius * 1.05);
-  grad.addColorStop(0, 'black');
-  grad.addColorStop(1, 'black');
-  ctx.strokeStyle = grad;
-  ctx.lineWidth = radius*0.1;
-  ctx.stroke();
-
-  ctx.beginPath();
-  ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
-  ctx.fillStyle = 'black';
-  ctx.fill();
+/**
+* drawTime(ctx, radius, cDate) - helper function to drawClock that creates the clock hand
+* Heavy inspiration from https://www.w3schools.com/graphics/canvas_clock_hands.asp
+*/
+function drawTime(ctx, radius, cDate){
+    var hours = cDate.getHours();
+    var minutes = cDate.getMinutes();
+    var seconds = cDate.getSeconds();
+    hours = hours % 12;
+    hours = (hours * Math.PI / 6) + (minutes * Math.PI / (6 * 60))
+    + (seconds * Math.PI / (360 * 60));
+    drawHand(ctx, hour, radius * 0.5, radius * 0.07);
+    minutes = (minutes * Math.PI / 30) + (seconds * Math.PI / (30 * 60));
+    drawHand(ctx, minutes, radius * 0.8, radius * 0.07);
+    seconds = (seconds * Math.PI / 30);
+    drawHand(ctx, seconds, radius * 0.9, radius * 0.02);
 }
-function drawNumbers(ctx, radius) {
-  var ang;
-  var num;
-  ctx.font = "bold " + radius * 0.15 + "px arial";
-  ctx.textBaseline = "middle";
-  ctx.textAlign = "center";
-  for(num = 1; num <= 12; num++){
-    ang = num * Math.PI / 6;
-    ctx.rotate(ang);
-    ctx.translate(0, -radius * 0.85);
-    ctx.rotate(-ang);
-    ctx.fillText(num.toString(), 0, 0);
-    ctx.rotate(ang);
-    ctx.translate(0, radius * 0.85);
-    ctx.rotate(-ang);
-  }
+/**
+* drawClockHand(ctx, pos, length, width) - helper function to drawClock that draws the clock hand
+* Heavy inspiration from https://www.w3schools.com/graphics/canvas_clock_hands.asp
+*/
+function drawClockHand(ctx, pos, length, width) {
+    ctx.beginPath();
+    ctx.lineCap = "round";
+    ctx.lineWidth = width;
+    ctx.moveTo(0, 0);
+    ctx.rotate(pos);
+    ctx.lineTo(0, -length);
+    ctx.stroke();
+    ctx.rotate(-pos);
 }
-function drawTime(ctx, radius, now){
-  var hour = now.getHours();
-  var minute = now.getMinutes();
-  var second = now.getSeconds();
-  //hour
-  hour = hour%12;
-  hour = (hour*Math.PI/6)+(minute*Math.PI/(6*60))+(second*Math.PI/(360*60));
-  drawHand(ctx, hour, radius*0.5, radius*0.07);
-  //minute
-  minute = (minute*Math.PI/30)+(second*Math.PI/(30*60));
-  drawHand(ctx, minute, radius*0.8, radius*0.07);
-  // second
-  second = (second*Math.PI/30);
-  drawHand(ctx, second, radius*0.9, radius*0.02);
-}
+/**
+* addzero(string s) - helper function that adds a leading zero to time value < 10
+* returns string with '0' before it
+*/
 function addzero(s) {
-  if (s.length == 1) {
-    s = "0" + s;
-  }
-  return s;
+    if (s.length == 1) {
+        s = "0" + s;
+    }
+    return s;
 }
-function drawHand(ctx, pos, length, width) {
-  ctx.beginPath();
-  ctx.lineWidth = width;
-  ctx.lineCap = "round";
-  ctx.moveTo(0,0);
-  ctx.rotate(pos);
-  ctx.lineTo(0, -length);
-  ctx.stroke();
-  ctx.rotate(-pos);
-}
+/**
+* displayTime() - function displays the current time, updating every millisecond
+*/
 function displayTime() {
     var d = new Date();
     if (!timeStopped) {
@@ -131,7 +168,7 @@ function displayTime() {
     }
     ampm = "AM";
     var hrs = d.getHours();
-    if (hrs > 12) {
+    if (hrs >= 12) {
         hrs -= 12;
         ampm = "PM";
     }
@@ -143,18 +180,30 @@ function displayTime() {
     seconds = addzero(seconds);
     var milliseconds = d.getMilliseconds().toString();
     milliseconds = addzero(milliseconds);
-    var date = hours + ":" +  minutes + ":" +  seconds + "." + milliseconds + " " + ampm;
+    var date = hours + ":" +  minutes + ":" +  seconds
+              + "." + milliseconds + " " + ampm;
     time.innerHTML = date;
     refreshTime();
 }
+/**
+* refreshTime() - helper function for displayTime(), calling function every millisecond
+*/
 function refreshTime() {
     var refresh = 1;
     mytime = setTimeout('displayTime()', refresh);
 }
+/**
+* stopClocks() - updates global timeStopped boolean variable when clock is clicked
+*/
 function stopClocks() {
     timeStopped = true;
     currDate = new Date();
 }
+/**
+* level1() - Function asks user for the time, based on created canvas clock and displayed
+*            time. User must correctly enter time to advance to level2, or they will run
+*            out of brains and trigger gameOver() function.
+*/
 function level1() {
     display.setAttribute("style", "background-image: url(\"images/brain.png\");");
     display.innerHTML = "<h1 id=\"level\">LEVEL 1</h1>";
@@ -220,14 +269,16 @@ function level1() {
         var s = document.getElementsByClassName('seconds')[0].value;
         var ms = document.getElementsByClassName('milliseconds')[0].value;
         if (ampm == "PM") {
-            if (h == currDate.getHours() || h == (currDate.getHours() - 12)) {
+            if ((h == currDate.getHours() || h == (currDate.getHours() - 12))
+                && m == currDate.getMinutes() && s == currDate.getSeconds()
+                && ms == currDate.getMilliseconds()) {
                 level2();
             } else {
-                display.innerHTML = h;
                 loseBrain();
             }
         } else {
-            if ((h == currDate.getHours())) {
+            if (h == currDate.getHours() && m == currDate.getMinutes()
+                && s == currDate.getSeconds() && ms == currDate.getMilliseconds()) {
                 level2();
             } else {
               loseBrain();
@@ -235,29 +286,10 @@ function level1() {
         }
     });
 }
-/*function checkLevel1() {
-    checkLevel1.preventDefault();
-    var h = document.querySelector('hours');
-    var m = document.getElementsByClassName('minutes').value;
-    var s = document.getElementsByClassName('seconds').value;
-    var ms = document.getElementsByClassName('milliseconds').value;
-    if (ampm == "PM") {
-        if (h == currDate.getHours() || h == (currDate.getHours() - 12)) {
-            level2();
-        } else {
-            display.innerHTML = h;
-            loseBrain();
-        }
-    } else {
-        if ((h == currDate.getHours())) {
-            level2();
-        } else {
-          display.innerHTML = h;
-          display.innerHTML += currDate.getHours();
-          loseBrain();
-        }
-    }
-}
+/**
+* loseBrain() - function decreases global 'lives' variable and updates corresponding
+*               HTML. If lives reaches 0 gameOver() function is called. Function is
+*               called when user has incorrect level attempt.
 */
 function loseBrain() {
     lives--;
@@ -268,11 +300,26 @@ function loseBrain() {
         gameOver();
     }
 }
+/**
+* level2() -
+*/
 function level2() {
     display.setAttribute("style", "background-image: url(\"images/brain.png\");");
     display.innerHTML = "<h1 id=\"level\">LEVEL 2</h1>";
     display.innerHTML += "<h2 id=\"numLives\">" + lives + "</h2>";
 }
+/**
+* level3() -
+*/
+function level3() {
+    display.setAttribute("style", "background-image: url(\"images/brain.png\");");
+    display.innerHTML = "<h1 id=\"level\">LEVEL 3</h1>";
+    display.innerHTML += "<h2 id=\"numLives\">" + lives + "</h2>";
+}
+/**
+* gameOver() - function effectively ends the game, and is triggered when global 'lives'
+*              variable reaches 0. Has 'play again' button if user wants to restart game
+*/
 function gameOver() {
     display.setAttribute("style", "background-image: url(\"images/gameover.png\");");
     display.innerHTML = "<button class='playagain' onClick='window.location.reload();'>PLAY AGAIN</button>"
